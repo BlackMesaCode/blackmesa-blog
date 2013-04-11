da5451d09cf84c222c14f6a952093f482286ccce
Stephan Sum <sum@live.de>
2013-04-11 22:16:25 +0200

    Adapted variables.less and added first blog entry

---
+++ BlackFor/Controllers/EntriesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace BlackFor.Controllers
+{
+    public class EntriesController : Controller
+    {
+        //
+        // GET: /Entries/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
