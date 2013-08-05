8cddc1fa66397d12b0c46cb396c18922fc074972
Stephan Sum <sum@live.de>
2013-08-05 22:48:20 +0200

    Changed project to work with sqlexpress instead of localdb

--- BlackMesa/Controllers/AboutController.cs
+++ BlackMesa/Controllers/AboutController.cs
@@ -13,13 +13,15 @@ namespace BlackMesa.Controllers
 
         public ActionResult Index()
         {
-            if (ViewBag.CurrentLanguage == "de")
-                return RedirectToActionPermanent("Details", "Entry", new {Id = 1});
+//            if (ViewBag.CurrentLanguage == "de")
+//                return RedirectToActionPermanent("Details", "Entry", new {Id = 1});
+//
+//            if (ViewBag.CurrentLanguage == "en")
+//                return RedirectToActionPermanent("Details", "Entry", new { Id = 2 });
+//
+//            return HttpNotFound();
 
-            if (ViewBag.CurrentLanguage == "en")
-                return RedirectToActionPermanent("Details", "Entry", new { Id = 2 });
-
-            return HttpNotFound();
+            return View();
         }
 
     }
