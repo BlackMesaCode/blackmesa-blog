d2f7211e1c31c66174d4898267abf3535c66d94e
Stephan Sum <sum@live.de>
2013-12-02 02:04:28 +0100

    Restructured Content folder

--- BlackMesa.Blog.Main/App_Start/BundleConfig.cs
+++ BlackMesa.Blog.Main/App_Start/BundleConfig.cs
@@ -21,26 +21,28 @@ namespace BlackMesa.Blog.Main.App_Start
 //            const string jQueryCdnPath = "http://code.jquery.com/jquery-1.9.1.min.js";
 
             bundles.Add(new ScriptBundle("~/bundles/global")
-                .Include("~/Scripts/jquery-{version}.js", "~/Scripts/jquery-ui-1.10.2.custom.js", "~/Scripts/bootstrap.js", "~/Scripts/shCore.js", "~/Scripts/shAutoloader.js", "~/Scripts/shLegacy.js", "~/Scripts/jquery.taghandler.js", "~/Scripts/global.js", "~/Scripts/jquery.unobtrusive*", "~/Scripts/jquery.validate*", "~/Scripts/google-analytics.js"));
+                .Include("~/Content/scripts/jquery-{version}.js", "~/Content/scripts/jquery-ui-1.10.2.custom.js", "~/Content/bootstrap/js/bootstrap.js", "~/Content/scripts/shCore.js", "~/Content/scripts/shAutoloader.js", "~/Content/scripts/shLegacy.js", "~/Content/scripts/jquery.taghandler.js", "~/Content/scripts/global.js", "~/Content/scripts/jquery.unobtrusive*", "~/Content/scripts/jquery.validate*", "~/Content/scripts/google-analytics.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/admin")
-                .Include("~/Scripts/bootstrap-datetimepicker.js", "~/Scripts/jquery-autogrow-textarea.js", "~/Scripts/ace/ace.js", "~/Scripts/ace/theme-clouds.js", "~/Scripts/ace/mode-html.js"));
+                .Include("~/Content/bootstrap-datepicker/js/bootstrap-datepicker.js", "~/Content/scripts/jquery-autogrow-textarea.js", "~/Content/scripts/ace/ace.js", "~/Content/scripts/ace/theme-clouds.js", "~/Content/scripts/ace/mode-html.js"));
             
  
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr")
+                .Include("~/Content/scripts/modernizr-*"));
 
 
             // Style Bundles
 
             var mainBundle = new Bundle("~/Content/global")
-                .Include("~/Content/styles/bootstrap.less")
-                .Include("~/Content/styles/jquery-ui-1.10.2.custom.less")
-                .Include("~/Content/styles/shCoreDefault.less")
-                .Include("~/Content/styles/shThemeDefault.less")
-                .Include("~/Content/styles/jquery.taghandler.less");
+                .Include("~/Content/font-awesome/css/font-awesome.min.css")
+                .Include("~/Content/bootstrap/css/bootstrap.min.css")
+                .Include("~/Content/custom/less/custom.less")
+                .Include("~/Content/custom/less/jquery-ui-1.10.2.custom.less")
+                .Include("~/Content/custom/less/shCoreDefault.less")
+                .Include("~/Content/custom/less/shThemeDefault.less")
+                .Include("~/Content/custom/less/jquery.taghandler.less");
                 
             mainBundle.Transforms.Add(cssTransformer);
             mainBundle.Transforms.Add(cssMinifier);
@@ -48,15 +50,15 @@ namespace BlackMesa.Blog.Main.App_Start
             bundles.Add(mainBundle);
 
             var adminBundle = new Bundle("~/Content/admin")
-                .Include("~/Content/styles/bootstrap-datetimepicker.less")
-                .Include("~/Content/styles/bootstrap-tagmanager.less");
+                .Include("~/Content/bootstrap-datepicker/css/datepicker.css");
+                //.Include("~/Content/styles/bootstrap-tagmanager.less");
             adminBundle.Transforms.Add(cssTransformer);
             mainBundle.Transforms.Add(cssMinifier);
             adminBundle.Orderer = nullOrderer;
             bundles.Add(adminBundle);
 
 
-//            BundleTable.EnableOptimizations = true;  // executing this line will force bundling and minification by overwriting whatever stands in web.config
+            //BundleTable.EnableOptimizations = true;  // executing this line will force bundling and minification by overwriting whatever stands in web.config
 //            #if DEBUG
 //                BundleTable.EnableOptimizations = false;
 //            #endif
