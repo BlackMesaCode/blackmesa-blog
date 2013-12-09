1cccc8b2c3b633d02ec8e9a1008f32fe55ca79a3
Stephan Sum <sum@live.de>
2013-12-09 23:27:01 +0100

    Revert "Added bundling"
    This reverts commit 78fcfee649e78d03c130234d3fc1bc4992c2bf28.

--- BlackMesa.Website.Main/Global.asax.cs
+++ BlackMesa.Website.Main/Global.asax.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data.Entity;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 using BlackMesa.Blog.DataLayer.DbContext;
 using BlackMesa.Identity.DataLayer.DbContext;
@@ -32,7 +31,6 @@ namespace BlackMesa.Website.Main
             //Database.SetInitializer(new CreateDatabaseIfNotExists<BlogContext>());  
 
             AreaRegistration.RegisterAllAreas();
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
