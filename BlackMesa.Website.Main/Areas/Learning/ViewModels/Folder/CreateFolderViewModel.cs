cf796656065fdec8c9358bf9f63da16aa726d218
Stephan Sum <sum@live.de>
2013-12-22 00:48:59 +0100

    improved forms styling

--- BlackMesa.Website.Main/Areas/Learning/ViewModels/Folder/CreateFolderViewModel.cs
+++ BlackMesa.Website.Main/Areas/Learning/ViewModels/Folder/CreateFolderViewModel.cs
@@ -1,12 +1,14 @@
-﻿namespace BlackMesa.Website.Main.Areas.Learning.ViewModels.Folder
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlackMesa.Website.Main.Areas.Learning.ViewModels.Folder
 {
     public class CreateFolderViewModel
     {
-
         public string Id { get; set; }
 
         public string ParentFolderId { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
 
