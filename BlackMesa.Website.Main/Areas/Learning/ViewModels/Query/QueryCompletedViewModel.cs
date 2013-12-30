4d53e6de4333a96fe6793c57c1f8eb15506d7926
Stephan Sum <sum@live.de>
2013-12-30 16:18:40 +0100

    Ammend

---
+++ BlackMesa.Website.Main/Areas/Learning/ViewModels/Query/QueryCompletedViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BlackMesa.Website.Main.Models.Learning;
+
+namespace BlackMesa.Website.Main.Areas.Learning.ViewModels.Query
+{
+    public class QueryCompletedViewModel
+    {
+        public string FolderId { get; set; }
+        public int TotalCount { get; set; }
+        public int CorrectCount { get; set; }
+        public int PartlyCorrectCount { get; set; }
+        public int WrongCount { get; set; }
+
+    }
+}
---
+++ BlackMesa.Website.Main/Models/Learning/IndexCard.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlackMesa.Website.Main.Models.Learning
+{
+    [Table("Learning_IndexCards")]
+    public class IndexCard : Unit
+    {
+        [StringLength(255)]
+        public string Question { get; set; }
+
+        [StringLength(10000)]
+        [DataType(DataType.MultilineText)]
+        public string Answer { get; set; }
+
+        [StringLength(255)]
+        [DataType(DataType.MultilineText)]
+        public string Hint { get; set; }
+
+        [StringLength(10000)]
+        public string CodeSnipped { get; set; }
+
+        [StringLength(2083)]
+        public string ImageUrl { get; set; }
+
+    }
+}
